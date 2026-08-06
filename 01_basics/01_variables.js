29ecb23166003e0bfa9c24fbcf2075665c6c90e9
GitSanya7 <yadavsanya116@gmail.com>
2026-08-06 15:58:35 +0000

    01 basics

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,17 @@ const accountId=1234
 let accountEmail="sanya@gmail.com"
 var accountPassword="1234"
 accountCity="vns"
+let accountState;  //js gives output as UNDEFINED if the value is not assigned 
+
+//accountId=2222 not allowed coz constant cant be change its single only
+console.log(accountId)
+
+/*prefer not to use Var coz of the issue in
+block scope and functional scope*/
+
+accountEmail="hello@gmail.com"
+accountPassword="2121"
+accountCity="bengaluru"
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+//console.table([]) is used to run too many keywords at once
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict" //treat all js code as newer version
+//alert(3+3) //we r using nodejs not browser so it will not run
+console.log(3
+    +3) //code readibility should be high
+console.log("sanya")
+
+let name = "sanya"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique
+
+
+// object
+
+console.log(typeof undefined); //returns undefined [undefined itself is a type in js]
+console.log(typeof null); // returns object[object is a type of null]
