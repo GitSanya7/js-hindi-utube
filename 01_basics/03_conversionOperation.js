c7a6797c514b165d9ad9da7294779efb8c1b962d
GitSanya7 <yadavsanya116@gmail.com>
2026-08-08 05:38:16 +0000

    conversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score="44ab"
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber =Number(score) 
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let IsLoggedIn = true
+let BooleanIsLoggedIn= Boolean(IsLoggedIn)
+console.log(IsLoggedIn)
+//33=>number
+//"33ab"=>NaN
+//true=1 , false=0
+
+let isLoggedIn=null
+let booleanisLoggedIn = Boolean(isLoggedIn)
+console.log(booleanisLoggedIn)
+//string=true
+//""=false
+//null=false
+//1=true 0=false
+
+let someNumber=44
+let stringsomeNumber=String(someNumber)
+console.log(stringsomeNumber)
+console.log(typeof stringsomeNumber)
+
+
+
+
