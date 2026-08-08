6f1ade415fc5d11faf479a792e28072b4b67dea4
GitSanya7 <yadavsanya116@gmail.com>
2026-08-08 06:32:57 +0000

    data config

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -27,5 +27,44 @@ console.log(stringsomeNumber)
 console.log(typeof stringsomeNumber)
 
 
+//***************operations***********
+let value=7
+let negvalue=-value
+console.log(negvalue)
+
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2**2)
+console.log(2%2)
+
+let str1="hello"
+let str2=" sanya"
+let str3=str1+str2
+
+console.log(str3);
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+"2");
+console.log(1+2+"4");
+console.log("1"+2+4);
+console.log((1+2)+4*7);
+
+console.log(+true);
+console.log(+"");
+
+let gameCounter=3
+++gameCounter
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+
+
+
+
+
+
+
 
 
