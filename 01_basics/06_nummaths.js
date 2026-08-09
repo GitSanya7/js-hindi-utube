09d833aadc681a2c8c0746e585d4d8299f4d3ada
GitSanya7 <yadavsanya116@gmail.com>
2026-08-09 08:27:03 +0000

    done

---
+++ 01_basics/06_nummaths.js
@@ -0,0 +1,11 @@
+const score =700
+console.log(score)
+
+const balance=new Number(700)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toFixed(3))
+const e=23.659
+console.log(e.toPrecision(3))
+const u=1000000
+console.log(u.toLocaleString('en-IN'))
