bd49b11f47d8eddefda0cd8c9b853ab79c2ed894
GitSanya7 <yadavsanya116@gmail.com>
2026-08-09 08:05:08 +0000

    string

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+//const name="sanya"
+//const repo=1
+//console.log(name+repo);
+//console.log(`hello my name is ${name} and my repocount is ${repo}.`);
+const value = new String ("snaya")
+console.log(value [0])
+console.log(value.__proto__)
+console.log(value.toUpperCase())
+console.log(value.length)
+console.log(value.indexOf('s'))
+console.log(value.charAt(2))
+const s=value.substring(0,3)
+console.log(s)
+
+const hello =value.slice(-2,4)
+console.log(hello)
+
+const c = "                 sanyayadav             "
+console.log(c)
+console.log(c.trim())
+console.log(c.trimStart())
+
+console.log(c.trimEnd())
+
+const url="sanyayadav7.url"
+console.log(url.replace(7,116))
+console.log(url.includes("sanya"))
+ let v="sanya.is.a.girl"
+ console.log(v.split("."))
+
+
+
