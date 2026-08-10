6dc8a00c9a923d774e68f91db1d95adb30a59e35
GitSanya7 <yadavsanya116@gmail.com>
2026-08-10 07:47:07 +0000

    nummaths

--- 01_basics/06_nummaths.js
+++ 01_basics/06_nummaths.js
@@ -1,11 +1,39 @@
-const score =700
-console.log(score)
+//const score =700
+//console.log(score)
 
-const balance=new Number(700)
-console.log(balance)
-console.log(balance.toString())
-console.log(balance.toFixed(3))
-const e=23.659
-console.log(e.toPrecision(3))
-const u=1000000
-console.log(u.toLocaleString('en-IN'))
+//const balance=new Number(700)
+//console.log(balance)
+//console.log(balance.toString())
+//console.log(balance.toFixed(3))
+//const e=23.659
+//console.log(e.toPrecision(3))
+//////const u=1000000
+//console.log(u.toLocaleString('en-IN'))
+
+//++++++++++++++maths+++++++++++
+ console.log(Math)
+ console.log(Math.abs(-87))
+ console.log(Math.round(43.98))
+ console.log(Math.floor(3.4))
+ console.log(Math.ceil(3.4))
+ console.log(Math.min(23,4,34,3244))
+ console.log(Math.max(23,4,34,3244))
+ console.log(Math.sqrt(36))
+
+ console.log(Math.random())//its value will always btwn 0 and 1
+ console.log((Math.random()*10)+1)//more than 0
+ console.log(Math.floor(Math.random()*10)+1)//it will not give zero 
+
+/*not understood*/
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+ 
+
+
+
+
+ 
+ 
+
